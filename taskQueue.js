988959168dfdf6649fea44090e2a49b33e2b3cc3
Philippe Mimms <philippe.mimms@gmail.com>
2017-01-30 01:00:17 -0500

    Adds AMD support Removes bower.json

--- taskQueue.js
+++ taskQueue.js
@@ -1,47 +1,58 @@
-module.exports = function TaskQueue() {
-    /**
-     * Indicates whether the task queue has been destroyed
-     * @type    {Boolean}
-     */
-    this.isDestroyed = false;
+;(function(TaskQueue) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        /* CommonJS module. */
+        module.exports = TaskQueue();
+    } else if (typeof define === "function" && define.amd) {
+        /* AMD module. */
+        define(TaskQueue);
+    }
+}(function() {
+        return function TaskQueue() {
+            /**
+             * Indicates whether the task queue has been destroyed
+             * @type    {Boolean}
+             */
+            this.isDestroyed = false;
 
-    /**
-     * The queue of functions to invoke
-     * @type    {Promise}
-     * @private
-     */
-    this._queue = Promise.resolve();
+            /**
+             * The queue of functions to invoke
+             * @type    {Promise}
+             * @private
+             */
+            this._queue = Promise.resolve();
 
-    /**
-     * Appends the function arguments to the task queue in the given order
-     * @type    {Function}
-     * @return  {Promise}
-     */
-    this.append = function() {
-        for (var i = 0, n = arguments.length; i < n; i++) {
-            if (typeof arguments[i] !== 'function') {
-                continue;
-            }
-            this._queue = this._queue.then(function(cb, val) {
-                if (this.isDestroyed) {
-                    return val;
+            /**
+             * Appends the function arguments to the task queue in the given order
+             * @type    {Function}
+             * @return  {Promise}
+             */
+            this.append = function() {
+                for (var i = 0, n = arguments.length; i < n; i++) {
+                    if (typeof arguments[i] !== 'function') {
+                        continue;
+                    }
+                    this._queue = this._queue.then(function(cb, val) {
+                        if (this.isDestroyed) {
+                            return val;
+                        }
+                        return cb(val);
+                    }.bind(this, arguments[i]));
                 }
-                return cb(val);
-            }.bind(this, arguments[i]));
-        }
 
-        return this._queue;
-    };
+                return this._queue;
+            };
 
-    /**
-     * Dereferences instance properties and methods
-     * @type    {Function}
-     */
-    this.destroy = function() {
-        this.isDestroyed = true;
+            /**
+             * Dereferences instance properties and methods
+             * @type    {Function}
+             */
+            this.destroy = function() {
+                this.isDestroyed = true;
 
-        this.append = undefined;
-        this.destroy = undefined;
-        this._queue = undefined;
-    };
-};
+                this.append = undefined;
+                this.destroy = undefined;
+                this._queue = undefined;
+            };
+        };
+    })
+);
